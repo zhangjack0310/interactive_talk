da52b59cbfe7dc89df3a51341c40e25e7a1dc29f
zhanggeng <zhanggeng@tigerbrokers.com>
2017-11-29 19:47:27 +0800

    no message

--- models.py
+++ models.py
@@ -6,6 +6,10 @@ conn = MongoClient()
 db = conn.interactive
 
 def form_data():
+    '''
+    { "_id" : ObjectId("5a1e2b0c421aa91505202447"), "info" : [ { "物品" : "soap", "交通" : "walk", "web_id" : 1, "数量" : "3", "经办人" : "jack" }, { "物品" : "bow", "交通" : "bike", "web_id" : 2, "数量" : "2", "经办人" : "mary" }, { "物品" : "noodle", "交通" : "car", "web_id" : 3, "数量" : "5", "经办人" : "mary" }, { "数量" : "4", "物品" : "chopstick", "经办人" : "jack", "交通" : "walk", "web_id" : 5 }, { "数量" : "7", "物品" : "brush", "经办人" : "jack", "交通" : "walk", "web_id" : 6 }, { "数量" : "4", "物品" : "computer", "经办人" : "lily", "交通" : "subway", "web_id" : 7 } ], "user" : "laobzhang", "key" : [ "数量", "物品", "经办人", "交通" ] }
+    :return:
+    '''
     data = {'info': [{"数量": '3',"物品":'soap',"经办人": 'jack', '交通':'walk','web_id':1},
                    {"物品": 'bow', "数量": '2', "经办人": 'mary', '交通':'bike','web_id':2},
                    {"物品": 'noodle', "数量": '5', "经办人": 'mary', '交通': 'car','web_id':3}],
